dcdae59de795470836e10d17619264a92950047e
Gaetano Migliaccio <gaetano.migliaccio@tc-group.it>
2019-04-16 11:22:20 +0200

    fix course pagination

--- src/main/java/it/tcgroup/vilear/coursemanager/repository/CourseEMRepository.java
+++ src/main/java/it/tcgroup/vilear/coursemanager/repository/CourseEMRepository.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 @Repository
 public class CourseEMRepository {
@@ -21,45 +23,47 @@ public class CourseEMRepository {
                                                       String courseType, String specialInitiatives){
 
 
-        String sql = "SELECT c FROM CourseEntity c ";
+        String sql = "SELECT * FROM course c ";
 
         List<String> whereCondition = new LinkedList<>();
 
         if( courseTitle != null){
-            whereCondition.add("c.courseTitle = '" + courseTitle + "' ");
+            whereCondition.add("c.course_title = '" + courseTitle + "' ");
         }
         if( contentsArea != null){
-            whereCondition.add("c.contentsArea = '" + ContentsAreaCourseEnum.create(contentsArea) + "' ");
+            whereCondition.add("c.contents_area = '" + ContentsAreaCourseEnum.create(contentsArea) + "' ");
         }
         if( learnerType != null){
-            whereCondition.add("c.learnerType = '" + LearnerTypeCourseEnum.create(learnerType) + "' ");
+            whereCondition.add("c.learner_type = '" + LearnerTypeCourseEnum.create(learnerType) + "' ");
         }
         if( supplyModality != null){
-            whereCondition.add("c.supplyModality = '" + SupplyModalityCourseEnum.create(supplyModality) + "' ");
+            whereCondition.add("c.supply_modality = '" + SupplyModalityCourseEnum.create(supplyModality) + "' ");
         }
         if( paymentModality != null){
-            whereCondition.add("c.paymentModality = '" + PaymentModalityEnum.create(paymentModality)+ "' ");
+            whereCondition.add("c.payment_modality = '" + PaymentModalityEnum.create(paymentModality)+ "' ");
         }
         if( foundsType != null){
-            whereCondition.add("c.foundsType = '" + FoundsTypeCourseEnum.create(foundsType) + "' ");
+            whereCondition.add("c.founds_type = '" + FoundsTypeCourseEnum.create(foundsType) + "' ");
         }
         if( courseStartDate != null){
-            whereCondition.add("c.courseStartDate\\:\\:DATE = ('" + courseStartDate + "')\\:\\:DATE ");
+
+            whereCondition.add("c.course_start_date\\:\\:DATE = '"+ courseStartDate +"'\\:\\:DATE ");
+
         }
         if( partFullTime != null){
-            whereCondition.add("c.partFullTime = '" + PartFullTimeCourseEnum.create(partFullTime) + "'");
+            whereCondition.add("c.part_full_time = '" + PartFullTimeCourseEnum.create(partFullTime) + "' " );
         }
         if( courseCode != null){
-            whereCondition.add("c.courseCode = '" + courseCode + "' ");
+            whereCondition.add("c.course_code = '" + courseCode + "' ");
         }
         if( businessName != null){
-            whereCondition.add("upper(c.businessName) = upper('" + businessName + "') ");
+            whereCondition.add("upper(c.business_name) = upper('" + businessName + "') ");
         }
         if( courseType != null){
-            whereCondition.add("c.courseType = '" + CourseTypeEnum.create(courseType) + "' ");
+            whereCondition.add("c.course_type = '" + CourseTypeEnum.create(courseType) + "' ");
         }
         if( specialInitiatives != null){
-            whereCondition.add("c.specialInitiatives = '" + SpecialInitiativesCourseEnum.create(specialInitiatives) + "' ");
+            whereCondition.add("c.special_initiatives = '" + SpecialInitiativesCourseEnum.create(specialInitiatives) + "' ");
         }
 
         int i = 1;
@@ -74,7 +78,7 @@ public class CourseEMRepository {
         }
 
         sql += where;
-        Query query = em.createQuery(sql, CourseEntity.class);
+        Query query = em.createNativeQuery(sql, CourseEntity.class);
 
         return query.getResultList();
 
